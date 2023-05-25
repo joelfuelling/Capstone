625367faddc839aa31a4f2fabcc5f4b5f80173cf
Joel <JoelFuelling@gmail.com>
2023-05-25 16:45:54 -0500

    MERN built upto User Authentication

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,9 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm"
+export default function AuthPage() {
+    return (
+        <>
+        <h1>Sign Up</h1>
+        <SignUpForm />
+        </>
+    )
+}
---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,8 @@
+
+export default function NewOrderPage() {
+    return (
+        <>
+        <h1>New order page</h1>
+        </>
+    )
+}

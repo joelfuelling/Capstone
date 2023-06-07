e0169c6d2fd02ff01ea2b47a143c05acf416057a
Joel <JoelFuelling@gmail.com>
2023-06-07 13:36:14 -0500

    Heroku push attempt with deployment

--- server.js
+++ server.js
@@ -24,12 +24,24 @@ app.use('/api/courses', require('./routes/api/courses'))
 app.use('/api/users', require('./routes/api/users'))
     // The following "catch all" route (note the *) is necessary to return the index.html on all non-AJAX requests
     //? https://create-react-app.dev/docs/deployment/
+
+// ----------------- DEPLOYMENT -------------------
+
+
+// ----------------- DEPLOYMENT -------------------
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('oncourse/build"'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'oncourse', 'build', 'index.html' ))
+  })
+}
 //! Configure to use port 3001 instead of 3000 during development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)

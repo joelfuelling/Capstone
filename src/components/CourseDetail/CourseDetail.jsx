d5c7afdfa7796d5370032b1cbcd1a588660d5a68
Joel <JoelFuelling@gmail.com>
2023-06-08 19:00:12 -0500

    commit before attempt to break up date into components from detail page.

--- src/components/CourseDetail/CourseDetail.jsx
+++ src/components/CourseDetail/CourseDetail.jsx
@@ -1,8 +1,31 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
+import {parseISO} from 'date-fns'
 import EditCourseForm from './EditCourseForm/EditCourseForm'
 
 // setCourse is added so that editForm can be passed the "new" course information.
 export default function CourseDetail({course, setCourse, handleDelete}){
+    const handleStartDate = () => {
+        const date = parseISO(course.startDate)
+        const month = date.toLocaleDateString('default', {month: 'short'})
+        const day = date.getDate()
+        const year = date.getFullYear()
+        const newStartDate = `${month}, ${day}, ${year}`
+        console.log(newStartDate)
+        return newStartDate
+    }
+    const newStartDate = handleStartDate()
+
+    const handleEndDate = () => {
+        const date = parseISO(course.endDate)
+        const month = date.toLocaleDateString('default', {month: 'short'})
+        const day = date.getDate()
+        const year = date.getFullYear()
+        const newEndDate = `${month}, ${day}, ${year}`
+        console.log(newEndDate)
+        return newEndDate
+    }
+    const newEndDate = handleEndDate()
+
     // editForm starts as false so that 'EDIT' appears first, rather than Close Editor
     const [editFormIsOpen, setEditFormIsOpen] = useState(false)
     function toggleEditForm(){
@@ -14,15 +37,17 @@ export default function CourseDetail({course, setCourse, handleDelete}){
     return (
         <>
         <div>
+            
             <h3>Course: {course.name}</h3>
             <p>Price: ${course.price}</p>  
-            <p>Start Date: {course.startDate}</p>
-            <p>End Date: {course.endDate}</p>
+            <p>Start Date: {newStartDate}</p>
+            <p>End Date: {newEndDate}</p>
             <p>Duration: {course.classLength}</p>
             <p>Class Days: {course.daysOfWeek}</p>
             {course.suppliesProvided && <p>Supplies Provided</p>}
             {course.recurring && <p>Recurring Classes</p>}
             <p>Description: {course.description}</p>
+            <hr></hr>
             <button onClick={handleDelete}>DELETE {course.name}</button>
             <button onClick={toggleEditForm}>
             {editFormIsOpen ? "Close Editor" : "EDIT"}
